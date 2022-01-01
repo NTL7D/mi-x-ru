75fe61e7296f161417a8b3711f3d5f1849c78a4b
NTL7D <55150110+NTL7D@users.noreply.github.com>
2022-01-01 15:19:20 +0700

    Add files via upload

--- michelle/script.js
+++ michelle/script.js
@@ -3,7 +3,8 @@ order.onclick = function(event) {
   location.replace("order.html");
 }
 
-function submitInfo() {
+document.getElementById("orderSubmit").addEventListener("click", submitOrder); 
+function submitOrder() {
   var name = document.getElementById("name").value;
   var phone = document.getElementById("phone").value;
   var mail = document.getElementById("mail").value;
@@ -11,11 +12,25 @@ function submitInfo() {
   var adress = document.getElementById("adress").value;
   if ( name == "" || phone == "" || mail == "" || food =="" || adress =="") {
      alert("Vui lòng nhập đầy đủ thông tin");
+     window.reload();
   } else {
      alert('Đặt hàng thành công!');
+  } 
+}
+
+function submitContact() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var mail = document.getElementById("mail").value;
+  var note = document.getElementById("note").value;
+  if ( name == "" || phone == "" || mail == "" || note =="") {
+     alert("Vui lòng nhập đầy đủ thông tin");
+  } else {
+    alert("Đã gửi thông tin thành công");
   }
 }
 
 function back(){
   location.replace("single.html");
 }
+
